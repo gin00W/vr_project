061db86f3a6d2f8564b0841ce036a049b7a11435
eeyezl <yeji030107@gmail.com>
2026-04-30 13:29:58 +0900

    방 꾸미기
    Room 3 화장실 완료

---
+++ Assets/Public Bathroom Assets/Scripts/objectInteraction.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Linq;
+using System.Collections;
+
+
+public class objectInteraction : MonoBehaviour {
+
+    public AudioSource soundEffect;
+    public Animator animatorObject;
+    //public Material material;
+    private Material[] materials;
+    public Color highlightColor;
+
+    public string animationName = "Interaction Animation";
+    public float minPitch = 0.9f;
+    public float maxPitch = 1.1f;
+
+    // Use this for initialization
+    void Start () {
+
+        materials = GetComponentsInChildren<Renderer>().SelectMany(r => r.materials).ToArray();
+        foreach (var tempMaterial in materials)
+        tempMaterial.SetColor("_EmissionColor", Color.black);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    void OnMouseUp()
+    {
+        if(animatorObject != null  &&  (soundEffect == null || !soundEffect.isPlaying))
+        {
+            animatorObject.Play(animationName);
+        }
+        if (soundEffect != null && !soundEffect.isPlaying)
+        {
+            soundEffect.pitch = Random.Range(minPitch, maxPitch);
+            soundEffect.Play();
+
+        }
+    }
+
+    void OnMouseEnter()
+    {
+        foreach(var tempMaterial in materials)
+        tempMaterial.SetColor("_EmissionColor", highlightColor);
+    }
+
+    void OnMouseExit()
+    {
+        foreach (var tempMaterial in materials)
+            tempMaterial.SetColor("_EmissionColor", Color.black);
+    }
+}
